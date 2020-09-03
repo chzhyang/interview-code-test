9cec29adc3059e49b0f8be16a87d964ec72d3dc0
chzhyang <chzhyangchn@gmail.com>
2020-09-03 20:59:02 +0800

    test template

---
+++ test/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Node{
+    int x;
+    Node(int y){
+        x=y;
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("hello");
+    }
+}
