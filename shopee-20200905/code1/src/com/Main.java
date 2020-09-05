b60a9a039d78b58e6deac771e1120653ad3aa18c
chzhyang <chzhyangchn@gmail.com>
2020-09-05 12:05:20 +0800

    shopee

--- shopee-20200905/code1/src/com/Main.java
+++ shopee-20200905/code1/src/com/Main.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class Main {
 
+	public static String camelCase(String s){
+		String[] tokens = s
+				.replaceAll("'|", "")
+				.split("[\\W_]+|(?<=[a-z])(?=[A-Z][a-z])");
+		s = "";
+		for(String token : tokens){
+			String lowercaseToken = token.toLowerCase();
+			s += tokens[0].equals(token)?lowercaseToken:lowercaseToken.toUpperCase().charAt(0)+lowercaseToken.substring(1);
+		}
+		return s;
+	}
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println();
-
-
-	public static void sollution() {
-		// TODO:
-		return;
+		String str = in.nextLine();
+		System.out.println(camelCase(str));
 	}
-
 }
--- shopee-20200905/code2/src/com/Main.java
+++ shopee-20200905/code2/src/com/Main.java
@@ -1,17 +1,49 @@
 package com;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
+	/**
+	 * 倒序查找，找到第一个数字x，条件是x后边的数字中有一个数字y<x
+	 */
+	public static StringBuilder solution(String s) {
+		StringBuilder sb = new StringBuilder(s);
+		int len = sb.length();//0-len-1
+		ArrayList<Integer> list = new ArrayList<>();
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		//Queue<Integer> maxH = new PriorityQueue<>(1000,(i1, i2) -> i2 - i1);//大根堆
+		for(int i=len-1; i>=0; i--){
+			int x = sb.charAt(i) - '0';
+			if(i==0){
+				max=x;
+				min=x;
+			}
+			else if(x <= min){
+				list.add(x);
+				min = x;
+				continue;
+			}else {//list中找到比x小的最大值
+				sb.setCharAt(i, (char)max);
+				list.add(x);
+				list.remove(max);
+				Collections.sort(list,(o1, o2) -> o2 - o1);//降序
+				int h = 0;
+				for(int j=i-1,; j<len; j++){
+					int t = list.get(h++);
+					sb.setCharAt(j,(char)t);
+				}
+			}
+		}
+		return sb;
+	}
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println();
+		String s = in.nextLine();
+		if(s!=null) {
+			System.out.println(solution(s));
+		}
 
-
-	public static void sollution() {
-		// TODO:
-		return;
 	}
 
 }
