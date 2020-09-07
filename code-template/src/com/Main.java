1fcd77eb2a8b5cd643d3578b7246866aeb09e09c
chzhyang <chzhyangchn@gmail.com>
2020-09-07 21:30:01 +0800

    update template

--- code-template/src/com/Main.java
+++ code-template/src/com/Main.java
@@ -6,10 +6,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for(int i=0; i<n; i++){
+
+		}
 		System.out.println();
+	}
 
-
-	public static void sollution() {
+	public static void solution() {
 		// TODO:
 		return;
 	}
